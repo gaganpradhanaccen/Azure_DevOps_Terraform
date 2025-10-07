b595886d911db93d51f004135b4db1b4879431d9
gaganpradhanaccen <gagan.pradhan@accenture.com>
2025-10-07 18:16:16 +0530

    Update main.tf

--- Infrastructure/Teraaform/main.tf
+++ Infrastructure/Teraaform/main.tf
@@ -1,35 +1,27 @@
-data "azurerm_client_config" "this" {}
-
-module "myfunction" {
-  source                            = "/function-app/azure"
-  version                           = "2.0.3"
-  airid                             = var.airid
-  environment                       = var.environment
-  location                          = var.location
-  function_name                     = var.function_name
-  zip_file                          = var.zip_file
-  resource_group                    = var.resource_group_name
-  function_language                 = var.function_language
-  plan_sku                          = var.plan_sku
-  auth_client_id                    = var.auth_client_id
-  auth_allowed_audiences            = var.auth_allowed_audiences
-  always_on                         = var.always_on
-  os_type                           = var.os_type
-  function_app_application_settings = var.function_app_application_settings
-  runtime_version                   = var.runtime_version
-  python_version                    = var.python_version
-  cors_input                        = var.cors_input
-  min_tls_cipher_suite              = var.min_tls_cipher_suite
+module "resource_group" {
+  source = "./module.tf"
+  resource_group_name = var.resource_group_name
+  location = var.location
 }
 
-
-
-module "role_assignment_1" {
-  source                 = "/role-assignment/azure"
-  version                = "3.0.0"
-  service_principal_name = var.service_principal_name
-  user_added_role        = "Key Vault Administrator"
-  depends_on             = [module.myfunction]
-  #skip_service_principal_aad_check = true
+module "function_app" {
+  source = "./module.tf"
+  function_app_name = var.function_app_name
+  resource_group_name = module.resource_group.name
+  location = var.location
 }
 
+module "azure_sql" {
+  source = "./module.tf"
+  sql_server_name = var.sql_server_name
+  database_name = var.database_name
+  resource_group_name = module.resource_group.name
+  location = var.location
+}
+
+module "role_assignment" {
+  source = "./module.tf"
+  function_app_id = module.function_app.id
+  sql_server_id = module.azure_sql.id
+  role_definition_name = var.role_definition_name
+}
