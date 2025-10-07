d0507e23b0d835fb86f9b0f5c4cf7fb63b847db2
gaganpradhanaccen <gagan.pradhan@accenture.com>
2025-10-07 18:17:57 +0530

    Update outputs.tf

--- Infrastructure/Teraaform/outputs.tf
+++ Infrastructure/Teraaform/outputs.tf
@@ -1,11 +1,41 @@
 // # Resource group
 // output "resource_group_id" {
-//   description = "ID of the resource group created."
-//   value       = module.resource-group.resource_group_id
-// }
+//   value = azurerm_resource_group.this.id
 
-// output "resource_group_name" {
-//   description = "Name of the resource group created."
-//   value       = module.resource-group.resource_group_name
 // }
+// # function app
+// output "function_app_id" {
+//   value = azurerm_function_app.function_app.id
+// }
+// output "function_app_default_hostname" {
+//   value = azurerm_function_app.function_app.default_hostname
+// }
+// # Azure SQL Database
+// output "sql_server_id" {
+//   value = azurerm_sql_server.sql_server.id
+// }
+// output "sql_database_id" {   
+//   value = azurerm_sql_database.sql_database.id
+// }        
+// # Role Assignment
+// output "role_assignment_id" {    
+//   value = azurerm_role_assignment.function_app_sql_access.id
+// }    
+// output "role_assignment_principal_id" {    
+//   value = azurerm_role_assignment.function_app_sql_access.principal_id   
+// }
+// output "role_assignment_scope" {
+//   value = azurerm_role_assignment.function_app_sql_access.scope
+// }    
+
+
+
+
+
+
+
+
+
+
+
 
